cee222a67e7260bf114d9cd52ac1f157e1c55a7d
unknown <kumarrohitsingh824@gmail.com>
2023-09-21 18:56:25 +0530

    m33

--- models/order.js
+++ models/order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
+  books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bok' }],
   totalAmount: Number,
 });
 
